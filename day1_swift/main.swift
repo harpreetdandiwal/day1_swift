635829fbe7229481bf5e39c161b5c506a096347d
harpreet <dandiwal357gmail.com>
2019-10-07 20:37:39 -0400

    init

--- day1_swift/main.swift
+++ day1_swift/main.swift
@@ -10,3 +10,39 @@ import Foundation
 
 print("Hello, World!")
 
+
+var str = "Hello, playground"
+//str=100
+
+// var a: int=100
+
+print (str)
+
+var c = 20
+var b = 49
+var d = c+b
+
+var sum = "sum of \(c) and \(b) is \(d)"
+print("values : \(c),\(b) sum : \(d)")
+print (sum)
+var p="😍"
+print (p)
+var 🦘="kangroo"
+print(🦘)
+
+
+
+var x1 = (1, "harpreet","dandiwal")
+print (x1)
+print(x1.0)
+print(x1.1)
+print(x1.2)
+var x2 = (collgeId : 10, collgename :"lambton college", cityname:"toronto")
+var x3 = (x2.cityname )
+print(x3)
+var (_,collegename, cityname) = x2
+var h = stride(from: 10, to: 1, by:2)
+print(h)
+for i in 1...10{
+    print("2 * \(i) = \(2*i)")
+}
